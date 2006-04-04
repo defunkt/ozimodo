a1a771e8b202f648e3954cb162c9a0acc4ddc239
ozmm <ozmm@bbd52f5d-c10c-0410-8bd0-8d55da062a3b>
2006-04-04 06:37:37 +0000

    add a file to put non-type, custom helpers
    git-svn-id: svn://rubyforge.org/var/svn/ozimodo/trunk@89 bbd52f5d-c10c-0410-8bd0-8d55da062a3b

---
+++ components/your_tumblelog/your_tumble_helper.rb
@@ -0,0 +1,5 @@
+# helpers you put in here will be accessible to your tumblelog and are 
+# super portable.  if you're looking for the core ozi helpers, check out
+# app/helpers/tumble_helper.rb
+module YourTumbleHelper  
+end
