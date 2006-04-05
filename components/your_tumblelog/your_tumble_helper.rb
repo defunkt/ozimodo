e0af97ceb46daec34c9ad13c6dd373bc4a2187c6
ozmm <ozmm@bbd52f5d-c10c-0410-8bd0-8d55da062a3b>
2006-04-05 04:55:08 +0000

    style and appearance tweaks to default layout
    git-svn-id: svn://rubyforge.org/var/svn/ozimodo/trunk@102 bbd52f5d-c10c-0410-8bd0-8d55da062a3b

--- components/your_tumblelog/your_tumble_helper.rb
+++
@@ -1,5 +0,0 @@
-# helpers you put in here will be accessible to your tumblelog and are 
-# super portable.  if you're looking for the core ozi helpers, check out
-# app/helpers/tumble_helper.rb
-module YourTumbleHelper  
-end
