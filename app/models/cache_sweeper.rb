e7325e88d2e820a7852dc23537602529d9cc9eeb
ozmm <ozmm@bbd52f5d-c10c-0410-8bd0-8d55da062a3b>
2006-05-01 06:44:20 +0000

    new caching model
    git-svn-id: svn://rubyforge.org/var/svn/ozimodo/trunk@139 bbd52f5d-c10c-0410-8bd0-8d55da062a3b

--- app/models/cache_sweeper.rb
+++ app/models/cache_sweeper.rb
@@ -1,45 +1,23 @@
+#
+# we destroy the entire cache whenever a change is made.
+# our caching model is inspired by http://www.fngtps.com/2006/01/lazy-sweeping-the-rails-page-cache
+#
 class CacheSweeper < ActionController::Caching::Sweeper
   observe Post, Tag
 
-  # this method is invoked is a Post or Tag is saved in certain methods we
-  # registered within app/controllers/admin/post_controller.rb
   def after_save(record)
-    if record.is_a?(Post)
-      # we're saving a new post.  get its id.
-      id = record.id
-      
-      # clear out tag list caches
-      expire_fragment /all_tags/
-      expire_fragment /list_tags/
-      expire_fragment /popular_tags/      
-      
-      # pagination
-      expire_fragment /pagination_links/
-      
-      # clear out the cache for that post's day
-      date = record.created_at
-      date_id = sprintf("show_date_%d-%02d-%02d", date.year, date.month, date.day )
-      expire_fragment :controller => '/tumble', :action => 'cache', :id => date_id
-      
-      # clear out the cache for that post's month
-      date_id = sprintf("show_month_%d-%02d", date.year, date.month )
-      expire_fragment :controller => '/tumble', :action => 'cache', :id => date_id
-      
-      # expire the cache for this posts page
-      expire_fragment :controller => '/tumble', :action => 'cache', :id => id    
-      
-      # grab the 20 most recent posts (those appearing on the front page)
-      # and paint an array of their ids
-      rposts = Post.find(:all, :order => 'created_at DESC', :limit => 20).map { |p| p.id }
-    
-      # if the post we're saving appears on the front page, we need to reset the
-      # feed caches as well as the recent_tags cache and the list_posts cache
-      if rposts.include?(id)
-        expire_page :controller => "/feed", :action => "rss.xml"
-        expire_page :controller => "/feed", :action => "atom.xml"
-        expire_fragment /list_posts/
-        expire_fragment :controller => '/tumble', :action => 'cache', :id => :recent_tags        
-      end
+    self.class.sweep
+  end
+
+  def after_destroy(record)
+    self.class.sweep
+  end
+
+  def self.sweep
+    cache_dir = ActionController::Base.page_cache_directory
+    unless cache_dir == "#{RAILS_ROOT}/public"
+      FileUtils.rm_r(Dir.glob("#{cache_dir}/*")) rescue Errno::ENOENT
+      RAILS_DEFAULT_LOGGER.info("Cache directory '#{cache_dir}' fully swept.")
     end
   end
 end
