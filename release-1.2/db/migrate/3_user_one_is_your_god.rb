f2e394293d3a6a033215e624fa28f759d4ca794f
defunkt <defunkt@bbd52f5d-c10c-0410-8bd0-8d55da062a3b>
2006-07-05 05:09:46 +0000

    whoops
    git-svn-id: svn://rubyforge.org/var/svn/ozimodo/trunk@282 bbd52f5d-c10c-0410-8bd0-8d55da062a3b

--- release-1.2/db/migrate/3_user_one_is_your_god.rb
+++
@@ -1,9 +0,0 @@
-class UserOneIsYourGod < ActiveRecord::Migration
-  def self.up
-    execute "ALTER TABLE posts MODIFY COLUMN user_id INT(11) DEFAULT 1"
-  end
-
-  def self.down
-    execute "ALTER TABLE posts MODIFY COLUMN user_id INT(11) DEFAULT NULL"
-  end
-end
