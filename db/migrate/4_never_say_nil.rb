0b5b007cdd1ce7774e1baa2786760f6ccc98ced8
defunkt <defunkt@bbd52f5d-c10c-0410-8bd0-8d55da062a3b>
2006-07-10 01:31:28 +0000

    migration tweaks
    git-svn-id: svn://rubyforge.org/var/svn/ozimodo/trunk@311 bbd52f5d-c10c-0410-8bd0-8d55da062a3b

--- db/migrate/4_never_say_nil.rb
+++ db/migrate/4_never_say_nil.rb
@@ -1,7 +1,7 @@
 class NeverSayNil < ActiveRecord::Migration
   def self.up
-    execute "ALTER TABLE posts MODIFY COLUMN title varchar(255) DEFAULT ''"
-    execute "ALTER TABLE posts MODIFY COLUMN content text DEFAULT ''"
+    execute "ALTER TABLE posts MODIFY COLUMN title varchar(255) NOT NULL DEFAULT ''"
+    execute "ALTER TABLE posts MODIFY COLUMN content text NOT NULL DEFAULT ''"
   end
 
   def self.down
