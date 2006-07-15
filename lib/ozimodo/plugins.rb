d3abf678ecc5834898c6bc7fa0f099b885ad2c8d
defunkt <defunkt@bbd52f5d-c10c-0410-8bd0-8d55da062a3b>
2006-07-15 07:21:56 +0000

    got rid of extra plugin class variable
    git-svn-id: svn://rubyforge.org/var/svn/ozimodo/trunk@416 bbd52f5d-c10c-0410-8bd0-8d55da062a3b

--- lib/ozimodo/plugins.rb
+++ lib/ozimodo/plugins.rb
@@ -1,13 +1,12 @@
 module Ozimodo
   class Plugins
-    @@plugin_paths, @@admin_links = {}, {}
+    @@admin_links = {}
     cattr_reader :admin_links
 
     class << self
 
       def register(plugin)
-        @@plugin_paths[plugin] = plugin_path = File.join(THEME_DIR, 'plugins', plugin.to_s)
-        Dir[File.join(plugin_path, '**', '**')].each do |file|
+        Dir[File.join(plugin_path(plugin), '**', '**')].each do |file|
           if file =~ /init\.rb$/ 
             load file
           else
@@ -21,10 +20,16 @@ module Ozimodo
         AdminController.send(:define_method, method) do
           block.call
           @page_name = method.to_s
-          file = File.join(@@plugin_paths[plugin], 'views', 'admin', "#{method.to_s}.rhtml")
+          file = File.join(plugin_path(plugin), 'views', 'admin', "#{method.to_s}.rhtml")
           raise "Can't find #{file} to render." unless File.exists?(file)
           render :file => file, :layout => nil
         end
+      end
+
+    protected
+
+      def plugin_path(plugin)
+        File.join(THEME_DIR, 'plugins', plugin.to_s)
       end
 
     end
