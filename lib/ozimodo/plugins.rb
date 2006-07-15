ccfca0ec8dbed8ae8d424860142a639f831e2c50
defunkt <defunkt@bbd52f5d-c10c-0410-8bd0-8d55da062a3b>
2006-07-15 08:02:20 +0000

    bug fix
    git-svn-id: svn://rubyforge.org/var/svn/ozimodo/trunk@418 bbd52f5d-c10c-0410-8bd0-8d55da062a3b

--- lib/ozimodo/plugins.rb
+++ lib/ozimodo/plugins.rb
@@ -17,10 +17,11 @@ module Ozimodo
 
       def admin_method(plugin, method, link_to_name = nil, &block)
         @@admin_links[method] = link_to_name ? link_to_name : method.to_s
+        plugin_path = File.join(plugin_path(plugin), 'views', 'admin', "#{method.to_s}.rhtml")
         AdminController.send(:define_method, method) do
           block.call
           @page_name = method.to_s
-          file = File.join(plugin_path(plugin), 'views', 'admin', "#{method.to_s}.rhtml")
+          file = plugin_path
           raise "Can't find #{file} to render." unless File.exists?(file)
           render :file => file, :layout => nil
         end
