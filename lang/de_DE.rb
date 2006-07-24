886558abe1eb51fe762550967597b371e545a2da
jannis <jannis@bbd52f5d-c10c-0410-8bd0-8d55da062a3b>
2006-07-24 01:25:37 +0000

    German localization strings
    git-svn-id: svn://rubyforge.org/var/svn/ozimodo/trunk@440 bbd52f5d-c10c-0410-8bd0-8d55da062a3b

---
+++ lang/de_DE.rb
@@ -0,0 +1,89 @@
+Localization.define('de_DE') do |l|
+
+  # script/../config/../config/../app/views/admin/password.rhtml
+  l.store 'Old Password', 'Altes Passwort'
+  l.store 'New Password', 'Neues Passwort'
+  l.store 'Confirm', 'Bestätigen'
+  l.store "Change Password", "Passwort ändern"
+
+  # script/../config/../config/../app/views/admin/list.rhtml
+  l.store 'Title', 'Titel'
+  l.store 'Type', 'Typ'
+  l.store 'User', 'Benutzer'
+  l.store 'Date', 'Datum'
+  l.store 'show', 'Zeigen'
+  l.store 'edit', 'Bearbeiten'
+  l.store 'delete', 'Löschen'
+  l.store 'Are you sure you want to delete this post?', 'Sind Sie sicher, dass Sie diesen Eintrag löschen wollen?'
+  l.store 'prev page', 'Zurück'
+  l.store 'next page', 'Weiter'
+  l.store 'listing posts', 'Einträge'
+
+  # script/../config/../config/../app/views/admin/layout.rhtml
+  l.store 'tumblelog maintenance', 'Tumbleblog Verwaltung'
+  l.store 'posts', 'Einträge'
+  l.store 'tags', 'Tags'
+  l.store 'tumble', 'Tumble'
+  l.store 'users', 'Benutzer'
+  l.store 'password', 'Passwort'
+  l.store 'kill cache', 'Cache löschen'
+  l.store 'tumblelog', 'Tumbleblog'
+  l.store 'logout', 'Logout'
+  l.store 'login', 'Login'
+  
+  # script/../config/../config/../app/views/admin/show.rhtml
+  l.store 'id', 'ID'
+  l.store 'user', 'Benutzer'
+  l.store 'title', 'Titel'
+  l.store 'post type', 'Typ'
+  l.store 'tags', 'Tags'
+  l.store 'created at', 'Datum'
+  l.store "show post", "Eintrag anzeigen"
+
+  # script/../config/../config/../app/views/admin/users.rhtml
+  l.store 'Users', 'Benutzer'
+  l.store 'listing users', 'Benutzer'
+  l.store 'rename', 'Umbenennen'
+  l.store 'password', 'Passwort'
+  l.store 'delete', 'Löschen'
+  l.store 'Are you sure you want to delete this user?  All his posts will be transferred to you.', 'Wollen Sie wirklich diesen Benutzer löschen? Einträge des Benutzer werden auf Sie übertragen.'
+  l.store 'create user', 'Benutzer anlegen'
+
+  # script/../config/../config/../app/views/admin/create_user.rhtml
+  l.store 'Name', 'Name'
+  l.store 'Password', 'Passwort'
+  l.store "create user", "Benutzer anlegen"
+  l.store 'Create User', 'Benutzer anlegen'
+
+  # script/../config/../config/../app/views/admin/up_to_date.rhtml
+  l.store "Version check failed.  Maybe you're not connected to the internets?", 'Versionsüberprüfung fehlgeschlagen. Besteht eventuell keine Verbindung zum Internet?'
+  l.store "I'm trying to grab ", "Lade herunter"
+  l.store "It's set in <em>rails_root/config/environment.rb</em>.  Maybe it's wrong?", 'In <em>rails_root/config/environment.rb</em> steht mehr. Ist da was falsch?'
+  l.store 'Your ozimodo is up to date.  Congratulations!', 'Ozimodo ist auf dem neusten Stand.  Glückwunsch!'
+  l.store 'Your ozimodo is not up to date.', 'Ozimodo ist nicht auf dem neusten Stand.'
+  l.store "The current version is", "Die aktuelle Version ist"
+  l.store "and you are running", "und es läuft gerade"
+  l.store 'Grab the freshest copy from', 'Laden Sie sich die neuste Version herunter:'
+  l.store 'am.i.up.to.date?', 'Versionsüberprüfung'
+
+  # script/../config/../config/../app/views/admin/login.rhtml
+  l.store 'User name', 'Benutzername'
+  l.store 'Remember me?', 'Auf diesem Computer merken.'
+  l.store 'You are already logged in.', 'Sie sind schon eingeloggt.'
+  l.store "login", "Login"
+  l.store 'Log out.', 'Logout'
+
+  # script/../config/../config/../app/views/admin/list_tags.rhtml
+  l.store 'Tags', 'Tags'
+  l.store 'listing tags', 'Tags'
+  l.store 'Are you sure you want to delete this tag?', 'Sind Sie sicher, dass sie diesen Tag löschen wollen?'
+  l.store 'save', 'Speichern' 
+
+  # script/../config/../config/../app/views/admin/new.rhtml
+  l.store 'Post Type', 'Typ'
+  l.store 'Content', 'Inhalt'
+  l.store 'Created at', 'Datum'
+  l.store 'tumble', 'Tumble!'
+  l.store 'Save Your Tumble', 'Eintrag speichern'
+  
+end
