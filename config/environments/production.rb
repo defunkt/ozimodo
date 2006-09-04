888e77894704c07e530f34028c49fb6bdc6a1110
defunkt <defunkt@bbd52f5d-c10c-0410-8bd0-8d55da062a3b>
2006-09-04 03:29:32 +0000

    turn page caching back on
    git-svn-id: svn://rubyforge.org/var/svn/ozimodo/trunk@450 bbd52f5d-c10c-0410-8bd0-8d55da062a3b

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,4 +6,4 @@ config.cache_classes = true
 # Full error reports are disabled and caching is turned off
 # Set perform_caching to 'true' if you want to use Rails' page caching feature.
 config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = false
+config.action_controller.perform_caching             = true
