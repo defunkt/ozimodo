89fe82e9d8fbcc387b33fd1863f3119c67578844
techpeace <techpeace@bbd52f5d-c10c-0410-8bd0-8d55da062a3b>
2007-07-03 22:52:57 +0000

    Added blip.tv support for video post type.
    git-svn-id: svn://rubyforge.org/var/svn/ozimodo/trunk@470 bbd52f5d-c10c-0410-8bd0-8d55da062a3b

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,4 +1,5 @@
 module AdminHelper
+=begin
   def write_field(type, field, hash, post, form)
     options = Hash.new
 
@@ -32,5 +33,38 @@ module AdminHelper
     #method + field + arguments_hash.to_s
     #send(method, field, content, options)
     form.send(method, field, options)
+  end
+=end
+
+  def write_field(type, field, hash, post)
+    options = Hash.new
+
+    method = case hash['type']
+             when 'textarea'
+               options[:cols] = hash['cols'] || 38
+               options[:rows] = hash['rows'] || 10
+               'text_area_tag'
+             when 'select'
+               hash['options'] = hash['options'].is_a?(Hash) ? hash['options'].invert : hash['options']
+               selected = post.post_type == type ? post.content.send(field) : nil
+               content = options_for_select(hash['options'], selected)
+               'select_tag'
+             when 'checkbox'
+               content = hash['options']
+               'checkbox_tag'
+             else
+               options[:size] = hash['size'] || 40
+               'text_field_tag'
+             end
+
+    content ||= if post.new_record? 
+                  (hash['default'] ? hash['default'] : '')
+                elsif post.post_type == type
+                  post.content.send(field)
+                else
+                  ''
+                end
+
+    send(method, "yaml[#{type}][#{field}]", content, options)
   end
 end
